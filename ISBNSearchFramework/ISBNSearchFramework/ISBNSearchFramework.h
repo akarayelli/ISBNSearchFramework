74531ffba33269c7d7f3ae8541c858c4b9596951
Alican Karayelli <alicankarayelli@gmail.com>
2017-09-17 12:44:16 +0300

    Initial commit  for framwork project

---
+++ ISBNSearchFramework/ISBNSearchFramework/ISBNSearchFramework.h
@@ -0,0 +1,19 @@
+//
+//  ISBNSearchFramework.h
+//  ISBNSearchFramework
+//
+//  Created by Alican Karayelli on 17/09/2017.
+//  Copyright © 2017 Alican Karayelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISBNSearchFramework.
+FOUNDATION_EXPORT double ISBNSearchFrameworkVersionNumber;
+
+//! Project version string for ISBNSearchFramework.
+FOUNDATION_EXPORT const unsigned char ISBNSearchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISBNSearchFramework/PublicHeader.h>
+
+

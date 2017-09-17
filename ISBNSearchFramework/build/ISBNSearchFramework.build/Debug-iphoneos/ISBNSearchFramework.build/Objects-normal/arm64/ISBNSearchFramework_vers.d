3d972624d4f1e4b1c61967d2713a3f44abea0eaa
Alican Karayelli <alicankarayelli@gmail.com>
2017-09-17 13:27:51 +0300

    deleted build files

--- ISBNSearchFramework/build/ISBNSearchFramework.build/Debug-iphoneos/ISBNSearchFramework.build/Objects-normal/arm64/ISBNSearchFramework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/alicankarayelli/Desktop/workspace/ISBNSearchFramework/ISBNSearchFramework/ISBNSearchFramework/build/ISBNSearchFramework.build/Debug-iphoneos/ISBNSearchFramework.build/DerivedSources/ISBNSearchFramework_vers.c
